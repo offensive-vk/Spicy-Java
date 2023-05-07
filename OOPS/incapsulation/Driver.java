11b0064942b82e6df39788a9436d05f8a04ec4f3
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-05-07 19:20:03 +0530

    Encapsulation

---
+++ OOPS/incapsulation/Driver.java
@@ -0,0 +1,15 @@
+package com.apex.icamp;
+
+public class Driver {
+
+	public static void main(String[] args) {
+	
+		Product p1=new Product();
+	
+		p1.setDetails(101, "Shirt", 1500);
+		
+		System.out.println(p1);
+
+	}
+
+}
---
+++ OOPS/incapsulation/Product.java
@@ -0,0 +1,30 @@
+package com.apex.icamp;
+
+public class Product {
+	private int pid;
+	@Override
+	public String toString() {
+		return "Product [pid=" + pid + ", name=" + name + ", price=" + price + "]";
+	}
+
+	private String name;
+    private float price;
+
+		public void setDetails(int pid, String name, float price) {
+			
+	    this.pid = pid;
+		this.name = name;
+		if(price<1000)
+		this.price =1000;
+		else
+			this.price=price;
+	}
+		
+	public void  printDetails()
+	{
+		System.out.printf("Pid:%d Name:%s Price:%f",pid,name,price);
+	}
+
+	
+
+}
