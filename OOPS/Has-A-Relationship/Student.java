33a5245ef9c2f8e8ef3788f3c91576587967c420
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-05-07 19:19:12 +0530

    Relationships

---
+++ OOPS/Has-A-Relationship/Student.java
@@ -0,0 +1,25 @@
+package com.apex.relationship;
+
+public class Student {
+	int sid;
+	String name;
+	float per;
+    Address add;
+	public Student(int sid, String name, float per, Address add) {
+		super();
+		this.sid = sid;
+		this.name = name;
+		this.per = per;
+		this.add = add;
+	}
+
+	String printDetails()
+	{
+		return sid+" "+name+" "+per+" "+add.printDetails();
+	}
+}
+
+
+
+
+
