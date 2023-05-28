3d50614cb7e5d6c7be0c80a0cdc2ae3ed932300a
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-05-28 17:44:33 +0530

    Abstract

---
+++ Abstract/AbstractDemo2.java
@@ -0,0 +1,36 @@
+package com.apex2.bcaa;
+abstract class MediaPlayer
+{
+	void playAudio() {System.out.println("can play audio");};
+	abstract void playVideo();
+}
+
+class VlcPlayer extends MediaPlayer
+{
+	public void playVideo()
+	{
+		System.out.println("can play video");
+	}
+}
+
+class SoundRecorder extends MediaPlayer
+{
+	public void playVideo()
+	{
+		System.out.println("can't play video");
+	}
+}
+
+public class AbstractDemo2 {
+
+	public static void main(String[] args) {
+		
+		VlcPlayer ob=new VlcPlayer();
+		ob.playVideo();
+		ob.playAudio();
+		
+		SoundRecorder ob1=new SoundRecorder();
+		ob1.playVideo();
+		ob1.playAudio();
+	    }
+}
---
+++ Abstract/MainClass.java
@@ -0,0 +1,11 @@
+package com.apex.bcaa.abstractdemo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		RegEmp ob=new RegEmp(101, "King", 50000, 7000);
+		ob.companyName();
+		ob.printDetails();
+	}
+
+}
