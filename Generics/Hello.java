7f2eeffd7b36256cbdde3da8a164a7f050365d2e
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-10-27 18:17:37 +0530

    ~

--- Generics/Hello.java
+++ Generics/Hello.java
@@ -1,11 +1,23 @@
 package Generics;
+
+import java.util.Map;
+
 public class Hello {
     public static <Type> void displayMessage(Type message) {
         System.out.println(message);
+    }
+    private static <X> X doSomething(X data){
+        String some = "Something";
+        System.out.println(some);
+        System.out.println("-> "+data+" \n ");
+        return data;
     }
     public static void main(String[] args) {
         displayMessage("Hello, Generics!");
         displayMessage(42);
         displayMessage(3.14);
+
+        doSomething(new Object());
     }
+    
 }
