913fea97b283429c7566e6a14bce5526222bba60
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-10-31 17:22:51 +0530

    java

--- Generics/GenericPair.java
+++ Generics/GenericPair.java
@@ -1,5 +1,14 @@
 package Generics;
 
 public class GenericPair {
-    
+        public static void main(String[] args) {
+        // Create a Pair of integers
+        Pair<Integer, Integer> intPair = new Pair<>(10, 20);
+        System.out.println("Pair of Integers: " + intPair.getFirst() + ", " + intPair.getSecond());
+
+        // Create a Pair of strings
+        Pair<String, String> stringPair = new Pair<>("Hello", "World");
+        System.out.println("Pair of Strings: " + stringPair.getFirst() + ", " + stringPair.getSecond());
+    }
 }
+
