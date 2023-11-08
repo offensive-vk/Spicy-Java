08d6d69c3260ec78fe0ff3e22d49b542589b71b0
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-08 19:00:05 +0530

    ~~

--- Generics/Interface.java
+++ Generics/Interface.java
@@ -1,5 +1,30 @@
 package Generics;
-
-public class Interface {
-    
+interface Pair<K, V> {
+    K getKey();
+    V getValue();
 }
+public class Interface <K, V> implements Pair<K, V>{
+    private K key;
+    private V value;
+
+    public Interface(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public static void main(String[] args) {
+        Pair<String, Integer> pair = new Interface<>("One", 1);
+        System.out.println("Key: " + pair.getKey());
+        System.out.println("Value: " + pair.getValue());
+    }
+}
+
+
