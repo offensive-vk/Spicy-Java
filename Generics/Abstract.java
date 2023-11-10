36e84fa870aeaeae00cdeb29c2f43b4e34e12fff
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-10 16:20:29 +0530

    ~~

--- Generics/Abstract.java
+++ Generics/Abstract.java
@@ -10,6 +10,7 @@ abstract class Human<T> {
     protected int data[];
     abstract void SomeRandom();
     abstract void SomeOtherRandom();
+    abstract int getLength();
 }
 
 class Parent<Type> extends Human<Integer> {
@@ -17,10 +18,12 @@ class Parent<Type> extends Human<Integer> {
         this.data = new int[100];
     }
     void SomeRandom(Type some){
-        this.data
         System.out.println(some);
     }
     void SomeOtherRandom(Type someOther){
         System.out.println(someOther);
+    }
+    final int getLength(){
+        return this.data.length;
     }
 }
