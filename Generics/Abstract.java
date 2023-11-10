1760436c64b0b251e9bf45bf6ae403fde7fb4dea
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-10 16:22:58 +0530

    Fix

--- Generics/Abstract.java
+++ Generics/Abstract.java
@@ -8,21 +8,24 @@ public final class Abstract {
 
 abstract class Human<T> {
     protected int data[];
-    abstract void SomeRandom();
-    abstract void SomeOtherRandom();
+    abstract void SomeRandom(T val);
+    abstract void SomeOtherRandom(T val);
     abstract int getLength();
 }
 
-class Parent<Type> extends Human<Integer> {
+class Parent<T> extends Human<T> {
     public Parent(){
         this.data = new int[100];
     }
-    void SomeRandom(Type some){
+    @Override
+    void SomeRandom(T some){
         System.out.println(some);
     }
-    void SomeOtherRandom(Type someOther){
+    @Override
+    void SomeOtherRandom(T someOther){
         System.out.println(someOther);
     }
+    @Override
     final int getLength(){
         return this.data.length;
     }
