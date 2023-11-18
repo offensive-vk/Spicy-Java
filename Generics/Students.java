69ba564e83f1e8ead70d4a908953f61f3ded104c
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-18 17:29:00 +0530

    ~~

--- Generics/Students.java
+++ Generics/Students.java
@@ -1,7 +1,9 @@
 package Generics;
 
 public class Students {
-    
+    public static void main(String[] args) {
+        
+    }
 }
 /**
  * @root Interface
@@ -10,7 +12,15 @@ public class Students {
 interface Student {
     String name = "";
     Integer id = 0;
+    String section = "";
 }
 interface RegularStudent<R> extends Student {
-
+    public abstract R setDetails();
+    public abstract R getDetails(RegularStudent<R> Student);
+    public abstract void addStudent(RegularStudent<R> Student);
+    public abstract void deleteStudent(RegularStudent<R> Student);
+}
+interface SpecialStudent<S> extends RegularStudent<S> {
+    String SpecialID = "";
+    public abstract S SpecialData(SpecialStudent<S> Student);
 }
