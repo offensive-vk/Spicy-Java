6cca664898e0df6581a083bcda8a4d1027ccbde5
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-19 17:23:59 +0530

    <>

--- Generics/Students.java
+++ Generics/Students.java
@@ -23,4 +23,37 @@ interface RegularStudent<R> extends Student {
 interface SpecialStudent<S> extends RegularStudent<S> {
     String SpecialID = "";
     public abstract S SpecialData(SpecialStudent<S> Student);
+    public abstract void setSpecialID(String ID);
+}
+
+class Management<M> implements SpecialStudent<M> {
+    
+    public String name = "";
+    public Integer id = 0;
+    public String section = "";
+
+    public Management(String n, Integer i, String s ){
+        this.name = n;
+        this.id = i;
+        this.section = s;
+    }
+    @Override
+    public M SpecialData(SpecialStudent<M> Student){
+        return null;
+    }
+    public void setSpecialID(String ID){
+
+    }
+    public final M setDetails() {
+        return null;
+    }
+    public final M getDetails(RegularStudent<M> Student) {
+        return null;
+    }
+    public final void addStudent(RegularStudent<M> Student) {
+
+    }
+    public final void deleteStudent(RegularStudent<M> Student) {
+
+    }
 }
