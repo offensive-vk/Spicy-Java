21f5c998fa02174f4d3e9eb1bd313d24e6ade21c
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-27 19:28:38 +0530

    ~~

--- Data_Structures/ArrayList.java
+++ Data_Structures/ArrayList.java
@@ -1,18 +1,8 @@
 package Data_Structures;
 
-import java.rmi.MarshalException;
-import java.util.HashMap;
-import java.util.LinkedList;
-
 public class ArrayList {
     public static void main(String[] args) {
-        ArrayList<Integer> L = new ArrayList();
-        
-        for(int i = 0; i < 15; i++){
-            L.add(i*57845);
-            
-        }
-        System.out.println(L);
+
 
     }
 }
