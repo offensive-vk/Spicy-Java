d07a64469d3ef48bf026ac4ff7c123a1d8ceac75
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-27 19:29:56 +0530

    %%

---
+++ Data_Structures/FirstList.java
@@ -0,0 +1,5 @@
+package Data_Structures;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
--- Data_Structures/List.java
+++
@@ -1,5 +0,0 @@
-package Data_Structures;
-
-public class List {
-    
-}
