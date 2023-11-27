d18b7324ffd48b0469aa0fa7ed3c671a12527ae9
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-27 17:43:13 +0530

    ~~

--- Data_Structures/First.java
+++ Data_Structures/First.java
@@ -1,5 +1,7 @@
 package Data_Structures;
 
 public class First {
-    
+    public static void main(String[] args) {
+        System.out.println("Package for Various Data Structures in Java. \n e.g (ArrayList, LinkedLists .etc)");
+    }
 }
