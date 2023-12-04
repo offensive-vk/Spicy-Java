905f91d2f2d12733e9cdfc6a51db8c387e208696
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-04 17:07:52 +0530

    Fixes

--- Data_Structures/First.java
+++ Data_Structures/First.java
@@ -3,7 +3,11 @@ package Data_Structures;
 public class First {
     public static void main(String[] args) {
         System.out.println("Package for Various Data Structures in Java. \n e.g (ArrayList, LinkedLists .etc)");
-        String[] names  = ["Arrays", "ArrayList", "LinkedList", "HashMap", "HashSet", "Queue", "Stack", "TreeMap", "PriorityQueue"];
         
+        String[] names  = {"Arrays", "ArrayList", "LinkedList", "HashMap", "HashSet", "Queue", "Stack", "TreeMap", "PriorityQueue"};
+
+        for(int c = 0; c < names.length; c++) {
+            System.out.printf("%d. %s\n", c, names[c]);
+        }
     }
 }
