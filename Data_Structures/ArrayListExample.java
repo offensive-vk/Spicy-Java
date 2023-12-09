bdaefe9d6335429e5e11a9b42d1993b80fc90a4b
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-09 20:51:44 +0530

    ***

--- Data_Structures/ArrayListExample.java
+++ Data_Structures/ArrayListExample.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class ArrayListExample {
     public static void main(String[] args) {
         List<Integer> ArrayList = new List<Integer>() {
-            
+            5342,76457,42724,82734, 74574
         };
     }
 }
