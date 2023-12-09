d6630c0919b2f1c6572408515e0fda5c22283051
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-09 20:48:09 +0530

    **

--- Data_Structures/ArrayListExample.java
+++ Data_Structures/ArrayListExample.java
@@ -1,7 +1,11 @@
 package Data_Structures;
 
+import java.util.List;
+
 public class ArrayListExample {
     public static void main(String[] args) {
-        
+        List<Integer> ArrayList = new List<Integer>() {
+            
+        };
     }
 }
