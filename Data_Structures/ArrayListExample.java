ec0f5b242e44487161962b232c99a2cad6f7ff90
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-15 12:17:46 +0530

    **

--- Data_Structures/ArrayListExample.java
+++ Data_Structures/ArrayListExample.java
@@ -2,10 +2,37 @@ package Data_Structures;
 
 import java.util.List;
 
+import java.util.ArrayList;
+
 public class ArrayListExample {
     public static void main(String[] args) {
-        List<Integer> ArrayList = new List<Integer>() {
-            5342,76457,42724,82734, 74574
-        };
+        // Create an ArrayList of Strings
+        ArrayList<String> fruits = new ArrayList<>();
+
+        // Add elements
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Orange");
+
+        // Access elements by index
+        System.out.println("First fruit: " + fruits.get(0));
+
+        // Modify elements
+        fruits.set(1, "Grapes");
+
+        // Iterate through the elements
+        System.out.println("All fruits:");
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+
+        // Check if an element is present
+        System.out.println("Contains Mango: " + fruits.contains("Mango"));
+
+        // Remove an element
+        fruits.remove("Orange");
+
+        // Size of the ArrayList
+        System.out.println("Number of fruits: " + fruits.size());
     }
 }
