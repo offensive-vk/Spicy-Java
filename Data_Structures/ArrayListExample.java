ee2006fb39ab9c931de99bae395378fb20bd4d49
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-15 13:02:58 +0530

    Fixes

--- Data_Structures/ArrayListExample.java
+++ Data_Structures/ArrayListExample.java
@@ -1,7 +1,5 @@
 package Data_Structures;
-
 import java.util.List;
-
 import java.util.ArrayList;
 
 public class ArrayListExample {
--- Data_Structures/First.java
+++ Data_Structures/First.java
@@ -3,7 +3,7 @@ package Data_Structures;
 public class First {
     public static void main(String[] args) {
         System.out.println("Package for Various Data Structures in Java. \n e.g (ArrayList, LinkedLists .etc)");
-        
+        System.out.println("\n========================\n");
         String[] names  = {"Arrays", "ArrayList", "LinkedList", "HashMap", "HashSet", "Queue", "Stack", "TreeMap", "PriorityQueue"};
 
         for(int c = 0; c < names.length; c++) {
