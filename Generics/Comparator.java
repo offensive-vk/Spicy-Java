2a68c2cc7488920d1ed223e78d5c56011868b0cb
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-20 18:34:43 +0530

    Comparator<T>

--- Generics/Comparator.java
+++ Generics/Comparator.java
@@ -9,3 +9,43 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+
+class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+public class ComparatorExample {
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("Alice", 30));
+        people.add(new Person("Bob", 25));
+        people.add(new Person("Charlie", 35));
+
+        // Using a Comparator to sort by age
+        Collections.sort(people, new Comparator<Person>() {
+            @Override
+            public int compare(Person p1, Person p2) {
+                return Integer.compare(p1.getAge(), p2.getAge());
+            }
+        });
+
+        // Display the sorted list
+        for (Person person : people) {
+            System.out.println(person.getName() + ", " + person.getAge() + " years old");
+        }
+    }
+}
