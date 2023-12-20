606a052cc82e5b88b663d33ab84034f016057a8f
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-20 18:24:10 +0530

    ~~

--- Generics/ComparableExample.java
+++ Generics/ComparableExample.java
@@ -17,3 +17,21 @@ class GenericBox<T extends Comparable<T>> {
         return this.value.compareTo(otherBox.getValue()) == 0;
     }
 }
+// A generic method with constraints
+class GenericMethod {
+    // This method accepts an array of objects that implement Comparable
+    static <T extends Comparable<T>> T findMax(T[] array) {
+        if (array == null || array.length == 0) {
+            return null;
+        }
+
+        T max = array[0];
+        for (T element : array) {
+            if (element.compareTo(max) > 0) {
+                max = element;
+            }
+        }
+        return max;
+    }
+}
+
