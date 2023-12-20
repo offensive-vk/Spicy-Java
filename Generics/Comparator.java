a32d747d44786357b3cd8680605b5b79040b9649
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-20 18:29:16 +0530

    ~~

---
+++ Generics/Comparator.java
@@ -0,0 +1,4 @@
+/**
+ * In Java, the Comparator interface is similar to Comparable in that it is used for comparing objects, but it provides a more flexible way to define custom ordering for classes that may not have a natural order or when you want to define multiple ways to order objects.
+ * 
+ */
