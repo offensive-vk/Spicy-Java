d677ac326f7b3082ae7d42fbec45afea9bf31b57
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-31 23:22:26 +0530

    <>

--- inheritance/Diamond.java
+++ inheritance/Diamond.java
@@ -1,5 +1,26 @@
 package inheritance;
 
-public class Diamond {
-    
+class A {
+    void display() {
+        System.out.println("Class A");
+    }
+}
+
+class B extends A {
+    void display() {
+        System.out.println("Class B");
+    }
+}
+
+class C extends A {
+    void display() {
+        System.out.println("Class C");
+    }
+}
+
+public class Diamond extends B, C { // Diamond Inheritance
+    public static void main(String[] args) {
+        D d = new D();
+        d.display();
+    }
 }
