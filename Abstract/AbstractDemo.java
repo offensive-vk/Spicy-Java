201c22abe50f506d9751c062df33245988aaa418
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-01-21 16:02:36 +0530

    DIR - Abstract

--- Abstract/AbstractDemo.java
+++ Abstract/AbstractDemo.java
@@ -1,62 +1,56 @@
 package Abstract;
 import java.util.Scanner;
 
-abstract class Shape
-{
-	Scanner sc=new Scanner(System.in);
-	public void Message()
-	{
+abstract class Shape {
+	Scanner sc = new Scanner(System.in);
+
+	public void Message() {
 		System.out.println("Abstract class demo");
 	}
+
 	abstract public void computeArea();
 }
 
-class Circle extends Shape
-{
-	public void computeArea()
-	{
+class Circle extends Shape {
+	public void computeArea() {
 		System.out.println("enter radius");
-		float radius=sc.nextFloat();
-		System.out.println("Area of circle="+(3.14*radius*radius));
+		float radius = sc.nextFloat();
+		System.out.println("Area of circle=" + (3.14 * radius * radius));
 	}
 }
-class Square extends Shape
-{
-	public void computeArea()
-	{
-		
+
+class Square extends Shape {
+	public void computeArea() {
+
 		System.out.println("enter side");
-		float side=sc.nextFloat();
-		System.out.println("Area of square="+(side*side));
+		float side = sc.nextFloat();
+		System.out.println("Area of square=" + (side * side));
 	}
 }
-class Triangle extends Shape
-{
-	public void computeArea()
-	{
+
+class Triangle extends Shape {
+	public void computeArea() {
 		System.out.println("enter base");
-		float base=sc.nextFloat();
+		float base = sc.nextFloat();
 		System.out.println("enter height");
-		float height=sc.nextFloat();
-		System.out.println("Area of triangle="+(.5*base*height));
+		float height = sc.nextFloat();
+		System.out.println("Area of triangle=" + (.5 * base * height));
 	}
 }
 
 public class AbstractDemo {
-
 	public static void main(String[] args) {
-		Shape ob=new Circle();
+		Shape ob = new Circle();
 		ob.Message();
 		ob.computeArea();
-		
-		ob=new Triangle();
+
+		ob = new Triangle();
 		ob.Message();
 		ob.computeArea();
-		
-		ob=new Square();
+
+		ob = new Square();
 		ob.Message();
 		ob.computeArea();
 
 	}
-
 }
--- Abstract/AbstractDemo2.java
+++ Abstract/AbstractDemo2.java
@@ -1,33 +1,32 @@
 package Abstract;
-abstract class MediaPlayer
-{
-	void playAudio() {System.out.println("can play audio");};
+
+abstract class MediaPlayer {
+	void playAudio() {
+		System.out.println("can play audio");
+	};
+
 	abstract void playVideo();
 }
 
-class VlcPlayer extends MediaPlayer
-{
-	public void playVideo()
-	{
+class VlcPlayer extends MediaPlayer {
+	public void playVideo() {
 		System.out.println("can play video");
 	}
 }
 
-class SoundRecorder extends MediaPlayer
-{
-	public void playVideo()
-	{
+class SoundRecorder extends MediaPlayer {
+	public void playVideo() {
 		System.out.println("can't play video");
 	}
 }
 
 public class AbstractDemo2 {
 	public static void main(String[] args) {
-		
+
 		VlcPlayer ob = new VlcPlayer();
 		ob.playVideo();
 		ob.playAudio();
-		
+
 		SoundRecorder ob1 = new SoundRecorder();
 		ob1.playVideo();
 		ob1.playAudio();
