25c221a588f4d074aeb2e0ba674c6c526457bdc1
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-01-21 18:47:05 +0530

    Inheritance

--- inheritance/DaillyWageEmployee.java
+++ inheritance/DaillyWageEmployee.java
@@ -1,26 +1,25 @@
-package inheritance;
-public class DaillyWageEmployee extends Employee {
-	
-float salary;
-float dailyWage;
-int noofdays;
-	
-	public DaillyWageEmployee(int eid, String name,float dailyWage,int noofdays) {
+package Inheritance;
+
+public class DailyWageEmployee extends Employee {
+
+	float salary;
+	float dailyWage;
+	int noofdays;
+
+	public DailyWageEmployee(int eid, String name, float dailyWage, int noofdays) {
 		super(eid, name);
-		this.dailyWage=dailyWage;
-		this.noofdays=noofdays;
-		salary=this.noofdays*this.dailyWage;
+		this.dailyWage = dailyWage;
+		this.noofdays = noofdays;
+		salary = this.noofdays * this.dailyWage;
 	}
 
 	@Override
 	public String toString() {
-		return "DaillyWageEmployee [salary=" + salary + ", dailyWage=" + dailyWage + ", noofdays=" + noofdays + ", eid="
+		return "DailyWageEmployee [salary=" + salary + ", dailyWage=" + dailyWage + ", noofdays=" + noofdays + ", eid="
 				+ eid + ", name=" + name + "]";
 	}
-	
-	void displayEmployeeType()
-	{
-		System.out.println("Dailly Wage Employee ");
-	}
 
+	void displayEmployeeType() {
+		System.out.println("Daily Wage Employee ");
+	}
 }
