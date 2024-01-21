2d9b38bdd3db2fdc74443977a11f3c3ae09f17f1
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-01-21 18:33:25 +0530

    DIR : Others

--- Others/ChainingDemo.java
+++ Others/ChainingDemo.java
@@ -1,42 +1,33 @@
 package Others;
 
-class Gparent
-{
-	Gparent(int num)
-	{
+class Gparent {
+	Gparent(int num) {
 		System.out.println("gparent parametrized constructor");
 	}
 }
 
-class Parent extends Gparent
-{
-	Parent()
-	{
-		super(10);//call to parameterized constructor of parent class
+class Parent extends Gparent {
+	Parent() {
+		super(10);// call to parameterized constructor of parent class
 		System.out.println("parent class constructor");
 	}
 }
 
-class Child extends Parent
-{
-	Child(int i)
-	{
+class Child extends Parent {
+	Child(int i) {
 		System.out.println("gparent parametrized constructor");
 	}
-	
-	Child()
-	{
-		this(10); //call to parameterized constructor of same class
+
+	Child() {
+		this(10); // call to parameterized constructor of same class
 		System.out.println("gparent default constructor");
 	}
-	
-	
 }
+
 public class ChainingDemo {
 
 	public static void main(String[] args) {
-		Child ob=new Child();
+		Child ob = new Child();
 
 	}
-
 }
