1bbc4c5acf5ffe8fcceeda3f14dc268b1d0aaf89
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-03-26 14:54:20 +0530

    Threads. Intro

---
+++ Threads/ThreadLifeCycle.java
@@ -0,0 +1,34 @@
+package Threads;
+
+class MyThread extends Thread {
+    public void run() {
+        System.out.println("Thread is running");
+    }
+}
+
+public class ThreadLifeCycle {
+    public static void main(String[] args) {
+        MyThread myThread = new MyThread();
+
+        System.out.println("Thread state after creation: " + myThread.getState());
+
+        myThread.start();
+        System.out.println("Thread state after starting: " + myThread.getState());
+
+        try {
+            Thread.sleep(1000); // Let the thread run for 1 second
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Thread state after 1 second: " + myThread.getState());
+
+        try {
+            myThread.join(); // Wait for the thread to finish
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Thread state after completion: " + myThread.getState());
+    }
+}
---
+++ Threads/ThreadStatesDemo.java
@@ -0,0 +1,34 @@
+package Threads;
+
+public class ThreadStatesDemo {
+    public static void main(String[] args) {
+        Thread thread = new Thread(() -> {
+            try {
+                Thread.sleep(1000); // Thread will be in timed waiting state for 1 second
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        System.out.println("Thread state after creation: " + thread.getState());
+
+        thread.start();
+        System.out.println("Thread state after starting: " + thread.getState());
+
+        try {
+            Thread.sleep(500); // Let the thread start before checking its state
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Thread state after 500ms: " + thread.getState());
+
+        try {
+            thread.join(); // Wait for the thread to finish
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Thread state after completion: " + thread.getState());
+    }
+}
